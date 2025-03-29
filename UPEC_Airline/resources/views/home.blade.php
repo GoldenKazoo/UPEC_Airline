11c2e8bd8971083ca5fc3a4627ada04f10718051
Zak <goldenkazoomail@gmail.com>
2025-03-29 22:06:17 +0100

    template + home

---
+++ UPEC_Airline/resources/views/home.blade.php
@@ -0,0 +1,3 @@
+@extends('template')
+@section('titre', 'Main page')
+@section('content')
---
+++ UPEC_Airline/resources/views/template.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('titre')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        body {
+            background: url('UPEC_Airline.png') no-repeat center center fixed;
+            background-size: cover;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            color: white;
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
